bae98374ee38ab82f808466c8b663100f63f8116
fp.kondado@gmail.com <fp.kondado@gmail.com>
2021-10-18 02:05:04 +0000

    Correções realizadas em chaves sk

--- models/northwind/mart/fact_orders.sql
+++ models/northwind/mart/fact_orders.sql
@@ -39,7 +39,7 @@ with
     o_details as (
         select
            order_id
-         , product_id                             
+         , product_sk as product_fk                             
          , unit_price
          , quantity
          , discount
@@ -51,19 +51,16 @@ with
 ,   orders_with_sk as (
         select
             orders.order_id
-          , customers.customer_id
-          , employees.employee_id          
-          , orders.order_date
-          , o_details.unit_price
-          , o_details.quantity
-          , o_details.discount         
+          , customers.customer_sk as customer_fk
+          , employees.employee_sk as employee_fk        
+          , orders.order_date                  
           , orders.ship_region
           , orders.shipped_date
           , orders.ship_country
           , orders.ship_address
           , orders.ship_postal_code
-          , orders.ship_city as cidade
-          , shippers.shipper_id
+          , orders.ship_city
+          , shippers.shipper_id as shipper_fk
           , orders.ship_name
           , orders.freight
           , orders.required_date
@@ -71,9 +68,33 @@ with
         from {{ ref('stg_orders') }} orders
 
         left join customers using(customer_id)
-        left join employees using(employee_id)
-        left join o_details using(order_id)        
+        left join employees using(employee_id)                
         left join shippers using(shipper_id)                
 )
 
-select * from orders_with_sk
+, final as (
+    select
+        o_details.order_id
+      , orders_with_sk.customer_sk 
+      , orders_with_sk.employee_sk         
+      , orders_with_sk.order_date
+      , o_details.product_fk
+      , o_details.unit_price
+      , o_details.quantity
+      , o_details.discount                  
+      , orders_with_sk.ship_region
+      , orders_with_sk.shipped_date
+      , orders_with_sk.ship_country
+      , orders_with_sk.ship_address
+      , orders_with_sk.ship_postal_code
+      , orders_with_sk.ship_city 
+      , orders_with_sk.shipper_fk 
+      , orders_with_sk.ship_name
+      , orders_with_sk.freight
+      , orders_with_sk.required_date
+
+      from orders_with_sk
+      left join o_details using(order_id)
+)
+
+select * from final
